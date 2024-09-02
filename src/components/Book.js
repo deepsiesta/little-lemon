88b61a234dc21c4643bb82c0bd38cbc376330379
Siesta <20047950+deepsiesta@users.noreply.github.com>
2024-09-02 23:19:38 +0200

    Refactor booking times

--- src/components/Book.js
+++ src/components/Book.js
@@ -18,23 +18,27 @@ const Book = (props) => {
         navigate("/success")
     }
 
+    const availableTimes = [
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+
+    ]
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="res-date">Choose date</label>
             <input type="date" id="res-date" value={date} onChange={e => setDate(e.target.value)} required />
             <label htmlFor="res-time">Choose time</label>
-            <select id="res-time " value={time} onChange={e => setTime(e.target.value)}>
-                <option>17:00</option>
-                <option>18:00</option>
-                <option>19:00</option>
-                <option>20:00</option>
-                <option>21:00</option>
-                <option>22:00</option>
+            <select id="res-time " value={time} placeholder={availableTimes[0]} onChange={e => setTime(e.target.value)} required>
+                {availableTimes.map(t => (<option>{t}</option>))}
             </select>
             <label htmlFor="guests">Number of guests</label>
             <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)} />
             <label htmlFor="occasion">Occasion</label>
-            <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)}>
+            <select id="occasion" value={occasion} placeholder="Birthday" onChange={e => setOccasion(e.target.value)}>
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
