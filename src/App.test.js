11d3cce57b7d6ef15f1fcd22bafc6e2d6a513b87
Siesta <20047950+deepsiesta@users.noreply.github.com>
2024-09-05 21:37:38 +0200

    Add static test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import Book from './components/Book.js';
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Renders the heading', () => {
+  render(
+    <BrowserRouter>
+      <Book availableTimes={[]} />
+    </BrowserRouter>
+  );
+  const headingElement = screen.getByText("Book Now");
+  expect(headingElement).toBeInTheDocument();
+})
--- src/components/Book.js
+++ src/components/Book.js
@@ -24,22 +24,25 @@ const Book = ({ availableTimes, dispatch, ...props }) => {
 
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="res-date">Choose date</label>
-            <input type="date" id="res-date" value={date} onChange={e => handleDateChange(e.target.value)} required />
-            <label htmlFor="res-time">Choose time</label>
-            <select id="res-time " value={time} placeholder={availableTimes[0]} onChange={e => setTime(e.target.value)} required>
-                {availableTimes.map(t => (<option>{t}</option>))}
-            </select>
-            <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)} />
-            <label htmlFor="occasion">Occasion</label>
-            <select id="occasion" value={occasion} placeholder="Birthday" onChange={e => setOccasion(e.target.value)}>
-                <option>Birthday</option>
-                <option>Anniversary</option>
-            </select>
-            <input type="submit" value="Make Your reservation" />
-        </form>
+        <>
+            <h2>Book Now</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="res-date">Choose date</label>
+                <input type="date" id="res-date" value={date} onChange={e => handleDateChange(e.target.value)} required />
+                <label htmlFor="res-time">Choose time</label>
+                <select id="res-time " value={time} onChange={e => setTime(e.target.value)} required>
+                    {availableTimes.map(t => (<option>{t}</option>))}
+                </select>
+                <label htmlFor="guests">Number of guests</label>
+                <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)} />
+                <label htmlFor="occasion">Occasion</label>
+                <select id="occasion" value={occasion} placeholder="Birthday" onChange={e => setOccasion(e.target.value)}>
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                </select>
+                <input type="submit" value="Make Your reservation" />
+            </form>
+        </>
     )
 }
 
